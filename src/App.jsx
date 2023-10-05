079f7f05d2f78e739975e5c8459be3300b6239e8
Ahmed-Magdy-Talaat <ahmed20227@feng.bu.edu.eg>
2023-10-05 23:53:57 +0300

    translate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "./containers/Languages";
-
 import "./App.css";
-
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
-
 import Home from "./pages/home/Home";
 import Career from "./pages/career/Career";
 import Services from "./pages/services/Services";
@@ -16,24 +13,24 @@ import Team from "./pages/team/Team";
 import Blog from "./pages/blog/Blog";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <LanguageProvider>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/career" element={<Career />} />
-                    <Route path="/services" element={<Services />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/team" element={<Team />} />
-                    <Route path="/blog" element={<Blog />} />
-                </Routes>
-                <Footer />
-            </LanguageProvider>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <LanguageProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+        <Footer />
+      </LanguageProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -13,19 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> ee5173ddf2cd9cde6b458f7650d95b4d664915e5
