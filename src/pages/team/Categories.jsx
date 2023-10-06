db47e18fab84585773a542de1251fd536c1c096c
radwa amer <rororado44@gmail.com>
2023-10-06 05:51:11 -0700

    create responsive categories bar

--- src/pages/team/Categories.jsx
+++ src/pages/team/Categories.jsx
@@ -8,11 +8,11 @@ const Categories = ({styles,handleCategories,cat}) => {
         setActive(item)
     }
     const categories=["all","managers","designers","mobile-developer","web-developer","non-technical","others"].map((c,index)=>{
-        return <li key={index} className={`${active==c?styles.active:""} rounded-3 p-3 m-0 flex-grow-1`} role="button" onClick={()=>handleClick(c)}><Text tid={"team.categories."+c} />{cat==active}</li>
+        return <li key={index} className={`${active==c?styles.active:""} rounded-3 p-3 m-0 flex-grow-1 w-100`} role="button" onClick={()=>handleClick(c)}><Text tid={"team.categories."+c} />{cat==active}</li>
     });
-    
+
     return (
-        <ul className={`${styles.categories} ${styles.grey} d-flex align-items-center border border-1 rounded-3 justify-content-between m-5 gap-1`}>
+        <ul className={`${styles.categories} ${styles.grey} d-flex flex-column flex-lg-row align-items-center border border-1 rounded-3 justify-content-between m-5 gap-1`}>
             {categories}
         </ul>
     )
