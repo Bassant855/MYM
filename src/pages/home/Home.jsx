27cec4a9fa831a81328e6903968dbfec3d046b13
moamed018 <93835752+moamed018@users.noreply.github.com>
2023-10-06 19:03:09 +0300

    Edit Portfolio folder name and images name in footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import Landing from "../../components/landing/Landing";
-import Portfolio from "../../components/portfolio/Portfolio";
+import Portfolio from "../../components/Portfolio/Portfolio";
 import Service from "../../components/service/Service";
 import Help from "../../components/help/Help";
 import Contact from "../../components/ContactUs/Contact";
@@ -10,18 +10,18 @@ import ClientsFeedback from "../../components/ClientsFeedback/ClientsFeedback";
 import Expert from "../../components/Expert/Expert";
 
 const Home = () => {
-  return (
-    <div>
-      <Landing />
-      <Service />
-      <Portfolio />
-      <Help />
-      <ClientsFeedback />
-      <Expert />
-      <Certificate />
-      <Contact />
-    </div>
-  );
+    return (
+        <div>
+            <Landing />
+            <Service />
+            <Portfolio />
+            <Help />
+            <ClientsFeedback />
+            <Expert />
+            <Certificate />
+            <Contact />
+        </div>
+    );
 };
 
 export default Home;
