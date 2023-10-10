9f69f01d260b161287da5a570ca05f8147ee1726
moamed018 <93835752+moamed018@users.noreply.github.com>
2023-10-10 12:13:53 +0300

    edits in home (last 4 sections)

--- src/components/ClientsFeedback/ClientsFeedback.jsx
+++ src/components/ClientsFeedback/ClientsFeedback.jsx
@@ -1,142 +1,156 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useContext } from "react";
 import style from "./ClientsFeedback.module.css";
-import { Text } from "../../containers/Languages";
+import { Text, LanguageContext } from "../../containers/Languages";
 import Slider from "react-slick";
 import fimg from "../../assets/Avatar4.png";
 import simg from "../../assets/Avatar3.png";
 function ClientsFeedback() {
-  const CustomPrevArrow = (props) => (
-    <div
-      className={`${style.arrow} ${style.left} justify-content-center align-content--center align-items-center`}
-      onClick={props.onClick}
-    >
-      <i className={`fa-solid fa-arrow-left ${style.icon}`}></i>
-    </div>
-  );
+    const { userLanguage } = useContext(LanguageContext);
 
-  const CustomNextArrow = (props) => (
-    <div
-      className={`${style.arrow} ${style.right} justify-content-center align-content-center align-items-center`}
-      onClick={props.onClick}
-    >
-      <i className={`fa-solid fa-arrow-right ${style.icon}`}></i>
-    </div>
-  );
-  const arr = [
-    {
-      id: 1,
-      name: "Mohamed",
-      name_ar: "محمد",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      description_ar:
-        "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
+    const CustomPrevArrow = (props) => (
+        <div
+            className={`${style.arrow} ${style.left} justify-content-center align-content--center align-items-center`}
+            onClick={props.onClick}
+        >
+            <i className={`fa-solid fa-arrow-left ${style.icon}`}></i>
+        </div>
+    );
 
-      pictureUrl: `${fimg}`,
-    },
-    {
-      id: 1,
-      name: "Mohamed",
-      name_ar: "محمد",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      description_ar:
-        "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
+    const CustomNextArrow = (props) => (
+        <div
+            className={`${style.arrow} ${style.right} justify-content-center align-content-center align-items-center`}
+            onClick={props.onClick}
+        >
+            <i className={`fa-solid fa-arrow-right ${style.icon}`}></i>
+        </div>
+    );
+    const arr = [
+        {
+            id: 1,
+            name: "Mohamed",
+            name_ar: "محمد",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            description_ar:
+                "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
 
-      pictureUrl: `${simg}`,
-    },
-    {
-      id: 2,
-      name: "Mohamed",
-      name_ar: "محمد",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      description_ar:
-        "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
+            pictureUrl: `${fimg}`,
+        },
+        {
+            id: 1,
+            name: "Mohamed",
+            name_ar: "محمد",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            description_ar:
+                "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
 
-      pictureUrl: `${simg}`,
-    },
-    {
-      id: 3,
-      name: "Mohamed",
-      name_ar: "محمد",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      description_ar:
-        "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
+            pictureUrl: `${simg}`,
+        },
+        {
+            id: 2,
+            name: "Mohamed",
+            name_ar: "محمد",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            description_ar:
+                "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
 
-      pictureUrl: `${simg}`,
-    },
-    {
-      id: 4,
-      name: "Mohamed",
-      name_ar: "محمد",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      description_ar:
-        "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
+            pictureUrl: `${simg}`,
+        },
+        {
+            id: 3,
+            name: "Mohamed",
+            name_ar: "محمد",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            description_ar:
+                "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
 
-      pictureUrl: `${fimg}`,
-    },
-    {
-      id: 5,
-      name: "Mohamed",
-      name_ar: "محمد",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-      description_ar:
-        "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
+            pictureUrl: `${simg}`,
+        },
+        {
+            id: 4,
+            name: "Mohamed",
+            name_ar: "محمد",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            description_ar:
+                "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
 
-      pictureUrl: `${simg}`,
-    },
-  ];
+            pictureUrl: `${fimg}`,
+        },
+        {
+            id: 5,
+            name: "Mohamed",
+            name_ar: "محمد",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            description_ar:
+                "خسائر اللازمة ومطالبة حدة بل. الآخر الحلفاء أن غزو, إجلاء وتنامت عدد مع. لقهر معركة لبلجيكا، بـ انه, ربع الأثنان المقيتة في, اقتصّت المحور حدة و. هذه ما طرفاً عالمية استسلام, الصين وتنامت حين ٣٠, ونتج والحزب المذابح كل جوي. أسر كارثة المشتّتون بل, وبعض وبداية الصفحة غزو قد, أي بحث تعداد الجنوب قصف المسرح واستمر الإتحاد في ذات أسيا للغزو ",
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: <CustomPrevArrow />,
-    nextArrow: <CustomNextArrow />,
-  };
-  return (
-    <div className={`py-3 my-3 ${style.bg} overflow-hidden`}>
-      <div className="mt-5 py-1  text-center fw-bold fs-1 ">
-        <Text tid="Client.title.black" />
-        <span className={`${style.fColor}`}>
-          <Text tid="Client.title.red" />
-        </span>
-      </div>
-      <div className={`${style.py}  col-9  m-auto`}>
-        <Slider {...settings}>
-          {arr.map((obj, indx) => {
-            return (
-              <div
-                className={`my-5 py-4 px-4 d-flex flex-column ${style.contain} w-75
-                  m-auto`}
-                key={indx}
-              >
-                <div className="d-flex flex-column flex-lg-row flex-md-row  align-items-center align-content-center gap-3">
-                  <div className={`${style.image}  `}>
-                    <img
-                      src={obj.pictureUrl}
-                      className={`w-100 ${style.image}`}
-                      alt={obj.name}
-                    />
-                  </div>
-                  <div className="fs-4">{obj.name}</div>
-                </div>
-                <div className={`mt-3 ${style.font} fs-5`}>
-                  {obj.description}
-                </div>
-              </div>
-            );
-          })}
-        </Slider>
-      </div>
-    </div>
-  );
+            pictureUrl: `${simg}`,
+        },
+    ];
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: <CustomPrevArrow />,
+        nextArrow: <CustomNextArrow />,
+    };
+    return (
+        <div className={`py-3 my-3 ${style.bg} overflow-hidden`}>
+            <div className="mt-5 py-1  text-center fw-bold fs-1 ">
+                <Text tid="Client.title.black" />
+                <span className={`${style.fColor}`}>
+                    <Text tid="Client.title.red" />
+                </span>
+            </div>
+            <div className={`${style.py}  col-9  m-auto`}>
+                <Slider {...settings}>
+                    {arr.map((obj, indx) => {
+                        return (
+                            <div
+                                className={`my-5 py-4 px-4 d-flex flex-column ${style.contain} w-75
+                  m-auto feedback-contain`}
+                                key={indx}
+                            >
+                                <div className="d-flex flex-column flex-lg-row flex-md-row align-items-center align-content-center gap-3">
+                                    <div className={`${style.image}  `}>
+                                        <img
+                                            src={obj.pictureUrl}
+                                            className={`w-100 ${style.image}`}
+                                            alt={
+                                                userLanguage === "en"
+                                                    ? obj.name
+                                                    : obj.name_ar
+                                            }
+                                        />
+                                    </div>
+                                    <div className="fs-4">
+                                        {userLanguage === "en"
+                                            ? obj.name
+                                            : obj.name_ar}
+                                    </div>
+                                </div>
+                                <div className={`mt-3 ${style.font} fs-5`}>
+                                    {userLanguage === "en"
+                                        ? obj.description
+                                        : obj.description_ar}
+                                </div>
+                            </div>
+                        );
+                    })}
+                </Slider>
+            </div>
+        </div>
+    );
 }
 
 export default ClientsFeedback;
